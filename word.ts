607ab64528fb52fa0c53cf563032f524a826ee69
shaheer718 <shaheeranwer718@gmail.com>
2024-04-17 16:26:47 +0500

    first commit

---
+++ word.ts
@@ -0,0 +1,23 @@
+#! usr/bin/env node
+
+// import the 'inquirer' module
+import inquirer from "inquirer"
+
+//Declaring a variable
+let answers: {
+    sentence: string
+} = await inquirer.prompt([
+    {
+       name: "sentence" ,
+       type: "input",
+       message: "Please enter your sentence to count the word: "
+    }
+])
+
+let words = answers.sentence.trim().split(" ")
+
+//Print array of words to the console
+console.log(words)
+
+//Print the word count of the sentence to the console
+console.log(`Dear User, Your sentence word count is ${words.length}`);
